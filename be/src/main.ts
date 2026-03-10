68cb621bdd915ff222eb74db20a7b9f880aad156
bilic <jopab03@gmail.com>
2026-03-10 22:00:19 +0100

    add entity modules

--- be/src/main.ts
+++ be/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder } from '@nestjs/swagger/dist/document-builder';
+import { SwaggerModule } from '@nestjs/swagger/dist/swagger-module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle("Movie explorer API")
+    .setDescription("The movie explorer API description")
+    .setVersion("1.0")
+    .addTag("movies")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
